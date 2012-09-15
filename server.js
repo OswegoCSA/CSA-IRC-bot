98a3ad81b9a6497e9a97bfeb121d17ab6ba24e02
Nicholas Poorman <nickpoorman@gmail.com>
2012-09-15 01:36:24 -0400

    jshintified

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 /*
  Oswego CSA IRC bot
  */
-var fs = require("fs")
+
+var fs = require("fs");
 try {
-  var config = JSON.parse(""+fs.readFileSync("config.json"));  
+  var config = JSON.parse(fs.readFileSync("config.json").toString());
 } catch (ex) {
-	console.log('got an error: %s', ex);
-    process.exit(1);
+  console.log('got an error: %s', ex);
+  process.exit(1);
 }
 
 
@@ -14,52 +15,59 @@ try {
 var nodemailer = require("nodemailer");
 
 // create reusable transport method (opens pool of SMTP connections)
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: config.mailer.service,
-    auth: {
-    	//name: "client server name",
-        user: config.mailer.user,
-        pass: config.mailer.pass
-    },
-    maxConnections: 1
+var smtpTransport = nodemailer.createTransport("SMTP", {
+  service: config.mailer.service,
+  auth: {
+    //name: "client server name",
+    user: config.mailer.user,
+    pass: config.mailer.pass
+  },
+  maxConnections: 1
 });
 // END Mailer ============================================================
-
 // setup e-mail data with unicode symbols
+
 function mailOptions(message) {
-	return {
-	    from: config.notify.from, // sender address
-	    to: config.notify.to, // list of receivers
-	    //replyTo: "" // replyTo address - don't really need it
-	    subject: config.notify.subject, // Subject line
-	    text: message || "You got an irc message", // plaintext body
-	    html: message || "You got an irc message" // html body	    
-	}	
+  return {
+    from: config.notify.from,
+    // sender address
+    to: config.notify.to,
+    // list of receivers
+    //replyTo: "" // replyTo address - don't really need it
+    subject: config.notify.subject,
+    // Subject line
+    text: message || "You got an irc message",
+    // plaintext body
+    html: message || "You got an irc message" // html body      
+  };
 }
 
 // This is just a mail queue that will check every 5 seconds 
 // and send the message if there is any
 var mailQueue = [];
-function sendMail(){
-	if(mailQueue.length != 0){		
-	    var message = mailQueue.shift();
-		smtpTransport.sendMail(message, function(error, response){
-			if(error){
-				console.log(error);
-				// add the message back to the queue cause to try again later
-				mailQueue.push(message);
-			}else{
-				console.log("Message sent: " + response.message);
-			}
-		    
-		    // I have to close it every time because the messages arrive out of order. 
-		    // If anyone has a better solution send a pull request.
-		    // if you don't want to use this transport object anymore, uncomment following line
-		    smtpTransport.close(); // shut down the connection pool, no more messages
-		})
-	}
+
+function sendMail() {
+  if (mailQueue.length !== 0) {
+    var message = mailQueue.shift();
+    smtpTransport.sendMail(message, function (error, response) {
+      if (error) {
+        console.log(error);
+        // add the message back to the queue cause to try again later
+        mailQueue.push(message);
+      } else {
+        console.log("Message sent: " + response.message);
+      }
+
+      // I have to close it every time because the messages arrive out of order. 
+      // If anyone has a better solution send a pull request.
+      // if you don't want to use this transport object anymore, uncomment following line
+      smtpTransport.close(); // shut down the connection pool, no more messages
+    });
+  }
 }
-setInterval(function(){ sendMail() }, 10000);
+setInterval(function() {
+  sendMail();
+}, 10000);
 
 /*
 * To set the key/cert explicitly, you could do the following
@@ -74,79 +82,75 @@ var options = {
 var irc = require('irc');
 var bot = new irc.Client(config.options.server, config.options.nick, config.options);
 
-bot.on('message#', function (from, to, message, raw) {
-    //console.log('<%s> %s', from, message);
-    if(message.match(/poorman/)){
-    	// send me an email or something
-    	console.log("got a message for me");    	
-		bot.emit('notify', message, from);
-    }
+bot.on('message#', function(from, to, message, raw) {
+  //console.log('<%s> %s', from, message);
+  if (message.match(/poorman/)) {
+    // send me an email or something
+    console.log("got a message for me");
+    bot.emit('notify', message, from);
+  }
 });
 
-bot.on('message', function (from, to, message) {
-    console.log('%s => %s: %s', from, to, message);
+bot.on('message', function(from, to, message) {
+  console.log('%s => %s: %s', from, to, message);
 
-    if ( to.match(/^[#&]/) ) {
-        // channel message
-        if ( message.match(/hello/i) ) {
-            bot.say(to, 'Hello there ' + from);
-        }
+  if (to.match(/^[#&]/)) {
+    // channel message
+    if (message.match(/hello/i)) {
+      bot.say(to, 'Hello there ' + from);
     }
-    else {
-        // private message        
-    }
+  } 
+  //else {
+    // private message        
+  //}
 });
 
-bot.on('notice', function (nick, to, text, message) {
-	if(nick === 'NickServ'){
-		// msg from nickserv		
-		if(text.match(/This nickname is registered. Please choose a different nickname/)){
-			// asking for ident
-			bot.emit('identify');
-		}
-	}    
+bot.on('notice', function(nick, to, text, message) {
+  if (nick === 'NickServ') {
+    // msg from nickserv        
+    if (text.match(/This nickname is registered\. Please choose a different nickname/)) {
+      // asking for ident
+      bot.emit('identify');
+    }
+  }
 });
 
-bot.on('identify', function () {
-	bot.say('NickServ', 'identify ' + config.ident.password);
+bot.on('identify', function() {
+  bot.say('NickServ', 'identify ' + config.ident.password);
 });
 
 
 bot.on('error', function(message) {
-    console.error('ERROR: %s: %s', message.command, message.args.join(' '));
+  console.error('ERROR: %s: %s', message.command, message.args.join(' '));
 });
 
-bot.on('notify', function(message, from){
-	//var chunkedMailOpts = [];
-	var userTag = '[' + from + ']> ';
-	if(config.notify.textMessage){		
-		var subjectSize = config.notify.subject.length + 3; // 3 for 2 paren and a space for the subject
-		var charLimit = 140;
-		var chunkSize = charLimit - subjectSize - userTag.length;
-		var re = new RegExp('.{1,' + chunkSize + '}\\W', "g");
-		var chunks = message.match(re);
-		for(var i = 0; i < chunks.length; i++){
-			mailQueue.push(mailOptions(userTag + chunks[i]));
-		}
-	} else {
-		mailQueue.push(mailOptions(userTag + message));
-	}
-})
+bot.on('notify', function(message, from) {
+  //var chunkedMailOpts = [];
+  var userTag = '[' + from + ']> ';
+  if (config.notify.textMessage) {
+    var subjectSize = config.notify.subject.length + 3; // 3 for 2 paren and a space for the subject
+    var charLimit = 140;
+    var chunkSize = charLimit - subjectSize - userTag.length;
+    var re = new RegExp('.{1,' + chunkSize + '}\\W', "g");
+    var chunks = message.match(re);
+    for (var i = 0; i < chunks.length; i++) {
+      mailQueue.push(mailOptions(userTag + chunks[i]));
+    }
+  } else {
+    mailQueue.push(mailOptions(userTag + message));
+  }
+});
 
 // See http://node-irc.readthedocs.org/en/latest/API.html#events
-
 // bot.on('pm', function (nick, message) {
 //     console.log('Got private message from %s: %s', nick, message);
 // });
-
 // bot.on('join', function (channel, who) {
 //     console.log('%s has joined %s', who, channel);
 // });
-
 // bot.on('part', function (channel, who, reason) {
 //     console.log('%s has left %s: %s', who, channel, reason);
 // });
-
 // bot.on('kick', function (channel, who, by, reason) {
 //     console.log('%s was kicked from %s by %s: %s', who, channel, by, reason);
 // });
